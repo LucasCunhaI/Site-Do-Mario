691fb3876bd382d3f2533a0c13aa51403bb32863
lucascunha2024 <lukascunha0@gmail.com>
2024-01-18 23:10:03 -0300

    responsividade do site mario

--- mario.js
+++ mario.js
@@ -3,13 +3,17 @@ const formulario = document.querySelector(".fale-conosco")
 
  const mascara = document.querySelector(".marcara-form")
 
+ const screen = {small: 0, medium: 200,  large: 500}
+
 
 function cliqueiNoBotao(){
     formulario.style.left = "60%"
     mascara.style.visibility = "visible"
+    screen = formulario.style.left = "20%"
 }
 
 function sumirFormulario(){
     formulario.style.left = "-22%"
     mascara.style.visibility = hidden
 }
+
